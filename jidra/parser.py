3f02a4297d2939c9ed04b2aba855d6b5380cf2b1
Akhil Singh <msingh09@outlook.com>
2026-06-23 01:30:13 -0700

    Add Go graph extraction support

--- jidra/parser.py
+++ jidra/parser.py
@@ -2,9 +2,15 @@ from __future__ import annotations
 
 from tree_sitter import Language, Parser
 import tree_sitter_java as tsjava
+import tree_sitter_go as tsgo
 
 JAVA_LANGUAGE = Language(tsjava.language())
+GO_LANGUAGE = Language(tsgo.language())
 
 
 def make_parser() -> Parser:
     return Parser(JAVA_LANGUAGE)
+
+
+def make_go_parser() -> Parser:
+    return Parser(GO_LANGUAGE)
